9888872ae0a75ef5771d782806ec402023235674
Unknown <laurence.ure@gmail.com>
2019-03-14 17:15:31 +0000

    S7L143 - Resource System

---
+++ Glitch Garden/Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour
+{
+	[SerializeField] int stars = 100;
+	Text starText;
+	// Use this for initialization
+	void Start()
+	{
+		starText = GetComponent<Text>();
+		UpdateDisplay();
+	}
+
+	private void UpdateDisplay()
+	{
+		starText.text = stars.ToString();
+	}
+
+	public void AddStars(int starAmount)
+	{
+		stars += starAmount;
+		UpdateDisplay();
+	}
+
+	public void RemoveStars(int starAmount)
+	{
+		if (stars >= starAmount)
+		{
+			stars -= starAmount;
+			UpdateDisplay();
+		}
+	}
+}
